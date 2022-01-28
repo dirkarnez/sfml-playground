48a0655b0bf76fae850559da91e84eded06a9952
dirkarnez <smalldirkalex@gmail.com>
2022-01-28 21:40:46 +0800

    - add cpk

--- local-cpk-build.cmd
+++ local-cpk-build.cmd
@@ -3,7 +3,11 @@ REM run as Administrator
 cd /d %~dp0
 set DOWNLOAD_DIR=%USERPROFILE%\Downloads
 set DOWNLOAD_DIR_LINUX=%DOWNLOAD_DIR:\=/%
-SET PATH=%DOWNLOAD_DIR%\PortableGit\bin;%DOWNLOAD_DIR%\mingw64-win32;%DOWNLOAD_DIR%\mingw64-win32\bin;%DOWNLOAD_DIR%\cmake-3.22.2-windows-x86_64\bin;%PATH%
+SET PATH=%DOWNLOAD_DIR%\PortableGit\bin;%DOWNLOAD_DIR%\mingw64-win32;%DOWNLOAD_DIR%\mingw64-win32\bin;%DOWNLOAD_DIR%\cmake-3.22.2-windows-x86_64\bin;P:\Downloads\gopath\src\github.com\dirkarnez\cpk
+
+set USER_NAME=dirkarnez
+
+cpk.exe
 
 cmake.exe -G"MinGW Makefiles" -B./build
 cd build
